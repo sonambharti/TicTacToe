64e50ea948881f10b6b81c66ec6613c3ab38c3e0
sonambharti <subodh9955509176@gmail.com>
2024-10-29 20:48:49 +0530

    Game functionality implemented

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset-btn"); 
+let newGamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
 
 // We will have 2 player: playerX, playerO
 let playerX = "X";
 let playerO = "O";
-
 let turn = playerX;
+let turnO = true;
 
 let gameOver = false;
 
@@ -20,21 +23,72 @@ const winPatterns = [
     [2, 4, 6],
 ]
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations🎉🎊, Player ${winner} wins!`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkForWin = () => {
+    for (pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != ""){
+            if (pos1val === pos2val && pos2val === pos3val){
+                if (pos1val === playerX) {
+                    alert("Player X wins!");
+                    showWinner(playerX);
+                } else if (pos1val === playerO) {
+                    alert("Player O wins!");
+                    showWinner(playerO)
+                }
+                gameOver = true;
+            }
+        }
+        if(gameOver){
+             
+            
+        }
+    }
+}
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (gameOver) return;
+        
+        // if (turnO) {
+        //     // playerO
+        //     box.textContent = "O";
+        //     turnO = false;
+        // } else {
+        //     // playerX
+        //     box.textContent = "X";
+        //     turnO = true;
+        // }
+        // box.disabled = true;
+        // checkForWin();
+        
         if (box.textContent === "") {
             console.log("Button clicked");
             box.textContent = turn;
+            box.disabled = true;
             checkForWin();
             turn = turn === playerX ? playerO : playerX;   
         }
+
+
         });
 });
+
 
 resetbtn.addEventListener("click", () => {
     boxes.forEach((box) => {
         box.textContent = "";
+        // turnO = true;
+        turn = playerX;
+        box.disabled = false;
+        gameOver = false;
         });
     
 })
